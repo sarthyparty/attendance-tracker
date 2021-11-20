fb311684826e5ee48584870d877d109206dc6564
Sameen A Rahman <sameenrahman45@gmail.com>
2021-11-19 22:02:04 -0600

    updated home screen

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import "./Home.css";
+import Typewriter from "typewriter-effect";
 
 
 function Login(props) {
@@ -15,16 +17,27 @@ function Login(props) {
     <div class = "home">
       <br />
       <br />
-      <div>
-        Code
-        <br />
-        <input type="text" {...code} autoComplete="new-password" />
+      <div class="label">
+            <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+              .typeString("Welcome!")
+              .stop()
+              .start();}}
+            />
       </div>
-      <input
-        type="button"
-        value={"Join"}
-        onClick={join}
-      />
+      <div>
+        <br />
+        <div class="box" style={{width:300, height:100}}>
+        <input type="text" placeholder="Type in code ..." {...code} autoComplete="new-password" />
+        <br />
+        <br />
+        </div>
+      </div>
+      <br />
+      <div class = "join_button" style={{width:200, height:100}}>
+      <input type="button" value={"Join"} onClick={join}/>
+      </div>
       <br />
       <Link to="/login">Login</Link>
     </div>
